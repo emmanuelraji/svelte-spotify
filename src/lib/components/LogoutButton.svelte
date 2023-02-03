4f3079db2e4bc28bbdb0442d097024c08e96d94d
Emmanuel Raji <ertech171@gmail.com>
2023-02-03 10:52:23 -0500

    create logout button component

---
+++ src/lib/components/LogoutButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+
+	async function handleLogout() {
+		const response = await fetch('/api/auth/logout', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json'
+			}
+		});
+		if (response.ok) {
+			invalidateAll();
+		}
+	}
+</script>
+
+<form action="/api/auth/logout" method="POST" on:submit|preventDefault={handleLogout}>
+	<button>Logout</button>
+</form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import type { LayoutData } from './$types';
+	import { LogoutButton } from '$components';
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
+
+	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
+
+{#if user}
+	<p>Hello, {user.display_name}</p>
+	<LogoutButton />
+{/if}
 
 <slot />
 
